4636186e373fcbf0ee649c76ead4afd00150792e
eduar <eduar@DESKTOP-SV6GQVL>
2023-03-13 23:00:12 -0600

    Agregar archivos de proyecto.

---
+++ Module1.vb
@@ -0,0 +1,36 @@
+﻿Imports System
+
+Interface IAnimal
+    Sub MakeSound()
+End Interface
+
+Public Class Dog
+    Implements IAnimal
+
+    Public Sub MakeSound() Implements IAnimal.MakeSound
+        Console.WriteLine("Woof!")
+    End Sub
+
+End Class
+
+Public Class Cat
+    Implements IAnimal
+
+    Public Sub MakeSound() Implements IAnimal.MakeSound
+        Console.WriteLine("Meow!")
+    End Sub
+
+End Class
+
+Public Class Program
+    Public Shared Sub AnimalSound(ByVal animal As IAnimal)
+        animal.MakeSound()
+    End Sub
+
+    Public Shared Sub Main()
+        Dim dog As IAnimal = New Dog()
+        Dim cat As IAnimal = New Cat()
+        AnimalSound(dog)
+        AnimalSound(cat)
+    End Sub
+End Class
---
+++ ProgramHelpers.vb
@@ -0,0 +1,9 @@
+﻿Class ProgramHelpers
+    Public Shared Sub AnimalSound(animal As IAnimal)
+        If animal Is Nothing Then
+            Throw New ArgumentNullException(NameOf(animal))
+        End If
+
+        animal.MakeSound()
+    End Sub
+End Class
